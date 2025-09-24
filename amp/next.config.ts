23fd09f0a69e9432ab201cd08bd549acec9908d8
Carlo Sacchi <sacchi.carlo@gmail.com>
2025-09-24 11:52:30 +0200

    refactor(tsconfig): remove JSON files from include list

--- amp/next.config.ts
+++ amp/next.config.ts
@@ -5,14 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config) => {
-    // Fix for JSON parsing issues
-    config.module.rules.push({
-      test: /\.json$/,
-      type: 'json',
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
